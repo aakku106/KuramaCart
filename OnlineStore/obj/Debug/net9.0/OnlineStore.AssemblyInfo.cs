fee750de7415ead751e0c290c0e812ce05204314
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-01-31 14:32:26 +0545

    Add Beauty and Kitchen product pages; update NavBar styling and include new images
    product added in choths component

--- OnlineStore/obj/Debug/net9.0/OnlineStore.AssemblyInfo.cs
+++ OnlineStore/obj/Debug/net9.0/OnlineStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95331d38fdb4c0a20734bbce7466adfafc721f24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98587eca5cc3b7fd364a8b7f35db45c28d18a6cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
