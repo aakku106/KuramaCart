1db62e27385466bc1de7eabb723185022a8413f7
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-04 22:29:12 +0545

    Update assembly information and add new kitchen images to static assets

--- OnlineStore/obj/Debug/net9.0/OnlineStore.AssemblyInfo.cs
+++ OnlineStore/obj/Debug/net9.0/OnlineStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e0a5c3c64e7aed07ff94cb497cd544c4b9c408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b0c783135e89d8aae3ceabc49be34b5451a760d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
