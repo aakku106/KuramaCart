25a9b9c8f11b7e041779187bc5ecbb28dab59c43
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-09 23:42:43 +0545

    Add new binary files and assets for OnlineStore release

--- OnlineStore/obj/Release/net9.0/OnlineStore.AssemblyInfo.cs
+++ OnlineStore/obj/Release/net9.0/OnlineStore.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4768a15336f93380904870127dccc61be1e1e663")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
