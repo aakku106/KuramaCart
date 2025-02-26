07992b48e4cbd515001b60ed837166ebd14740fd
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-27 00:45:07 +0545

    Remove nixpacks configuration and update assembly informational version for consistency

--- OnlineStore/obj/Debug/net9.0/OnlineStore.AssemblyInfo.cs
+++ OnlineStore/obj/Debug/net9.0/OnlineStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb5d85f26c4c809a765d5073a8a67ef090d84d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4376edad5d0ac4027b786708d047020ed79281")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
